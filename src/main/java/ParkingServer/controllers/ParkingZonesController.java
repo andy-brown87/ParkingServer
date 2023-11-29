f5640a65e20f1e338ce762c6d9841692d75c63dc
LidzDev <lydia.cordell@gmail.com>
2023-11-29 18:48:24 +0000

    added route for 1 parking zone

--- src/main/java/ParkingServer/controllers/ParkingZonesController.java
+++ src/main/java/ParkingServer/controllers/ParkingZonesController.java
@@ -2,14 +2,17 @@ package ParkingServer.controllers;
 
 
 import ParkingServer.models.ParkingZone;
+import ParkingServer.models.SpotCoordinate;
 import ParkingServer.repositories.ParkingZonesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class ParkingZonesController {
@@ -21,4 +24,9 @@ public class ParkingZonesController {
     public ResponseEntity<List<ParkingZone>> getAllParkingZones() {
         return new ResponseEntity<>(parkingZonesRepository.findAll(), HttpStatus.OK);
     }
+    @GetMapping(value = "/parkingzones/{id}")
+    public Optional<ParkingZone> getParkingZone(@PathVariable String id) {
+        return parkingZonesRepository.findById(id);
+    }
+
 }
